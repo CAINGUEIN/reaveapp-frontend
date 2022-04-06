e8e1c6e44260fd7733b5f47883a117d9914bf1d9
Robuchon Hugues <hugues.robuchon@gmail.com>
2022-04-06 20:37:07 +0200

    création des services

---
+++ src/services/authServices.js
@@ -0,0 +1,44 @@
+import Base from ('../plugins/axios/axiosPlugin')
+
+class AuthServices {
+    static async login(data) {
+        let body = data;
+        return Base.post(`/auth/login`, body)
+          .then((response) => {
+            console.log("auth : login", response);
+            return response;
+          })
+          .catch((e) => {
+            console.log("error ", e);
+            return e;
+          });
+    }
+
+    static async register(data) {
+        let body = data;
+        return Base.post(`/auth/register`, body)
+          .then((response) => {
+            console.log("auth : register", response);
+            return response;
+          })
+          .catch((e) => {
+            console.log("error ", e);
+            return e;
+          });
+      }
+    
+      static async logout() {
+        console.log("logout auth service called");
+        return Base.post(`/auth/logout`)
+          .then((response) => {
+            console.log("auth : logout", response);
+            return response;
+          })
+          .catch((e) => {
+            console.log("error ", e);
+            return e;
+          });
+      }
+}
+
+export default AuthServices;
---
+++ src/services/userServices.js
@@ -0,0 +1,37 @@
+import Base from "../plugins/axios-plugin";
+
+class UsersServices {
+  static async usersList(filter) {
+    let params = ""
+    if (filter !== undefined) {
+      params = filter
+    }
+    console.log("users service called");
+    // req
+    return Base.get(`/users`,{params})
+    .then((response) => {
+      console.log("service : usersList : res ", response);
+        return response;
+      })
+      .catch((e) => {
+        console.log("error ", e);
+        return e;
+      });
+  }
+
+  static async deleteUser(uuid) {
+    console.log("delete users service called");
+    // req
+    return Base.delete(`/users/` + uuid)
+    .then((response) => {
+      console.log("service : deleteUser : res ", response);
+        return response;
+      })
+      .catch((e) => {
+        console.log("error ", e);
+        return e;
+      });
+  }
+}
+
+export default UsersServices;
