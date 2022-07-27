fbab8bfdcfb8ba2d717041e72af4fc45bc4d5f66
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-07-27 13:08:35 +0200

    socket

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,8 @@ export default defineConfig({
       "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
       "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
     },
-  }, 
+  },
+    server: {
+      hmr: false
+  },  
 });
