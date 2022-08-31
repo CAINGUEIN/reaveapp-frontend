1fc578bef35a06485068d2933a62afa9eed842c1
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-08-31 16:19:50 +0200

    add chevron svg

---
+++ src/core/assets/icons/Chevron.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <svg
+      width="14"
+      height="9"
+      viewBox="0 0 14 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13 1L7 8"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1 1L7 8"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  </div>
+</template>
