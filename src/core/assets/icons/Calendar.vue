4196ed984787f365717ba499366391ee6bc99368
Théo Vidal <dalviorigine74@gmail.com>
2023-06-06 23:06:53 +0200

    [operator-ticket] change Calendar logo

--- src/core/assets/icons/Calendar.vue
+++ src/core/assets/icons/Calendar.vue
@@ -3,38 +3,56 @@
     <svg
       :width="width"
       :height="height"
-      viewBox="0 0 32 32"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <rect
-        x="4"
-        y="5.44434"
-        width="24"
-        height="22.5556"
-        rx="3"
+        x="1.48828"
+        y="2.47852"
+        width="17.0828"
+        height="16.0643"
+        rx="3.375"
         :stroke="color1"
-        stroke-width="2"
+        stroke-width="1.25"
         stroke-linejoin="round"
       />
       <path
-        d="M3.35938 12.3057H28.6372"
+        d="M1.11914 7.39453H18.9427"
         :stroke="color1"
-        stroke-width="2"
+        stroke-width="1.25"
         stroke-linejoin="round"
       />
       <path
-        d="M11 7V4"
+        d="M1.11914 12.9141H18.9427"
         :stroke="color1"
-        stroke-width="2"
-        stroke-linecap="round"
+        stroke-width="1.25"
+        stroke-linejoin="round"
       />
-      <path
-        d="M21 7V4"
+      <rect
+        x="6.14727"
+        y="1.16485"
+        width="0.665643"
+        height="2.8599"
+        rx="0.332822"
+        fill="white"
         :stroke="color1"
-        stroke-width="2"
-        stroke-linecap="round"
+        stroke-width="0.665643"
+        stroke-linejoin="round"
       />
+      <rect
+        x="13.2781"
+        y="1.16485"
+        width="0.665643"
+        height="2.8599"
+        rx="0.332822"
+        fill="white"
+        :stroke="color1"
+        stroke-width="0.665643"
+        stroke-linejoin="round"
+      />
+      <path d="M6.67773 7.5V18.3333" :stroke="color1" stroke-width="1.25" />
+      <path d="M13.3223 7.5V18.3333" :stroke="color1" stroke-width="1.25" />
     </svg>
   </div>
 </template>
@@ -44,11 +62,11 @@ export default {
   props: {
     width: {
       type: Number,
-      default: 32,
+      default: 20,
     },
     height: {
       type: Number,
-      default: 32,
+      default: 20,
     },
     color1: {
       type: String,
