6dfc3720c1763f42e1f174b69590790acc32f705
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-10-30 14:07:58 +0100

    corrected Bento icon color

--- src/core/assets/icons/Bento.vue
+++ src/core/assets/icons/Bento.vue
@@ -1,38 +1,57 @@
 <template>
   <div>
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <ellipse cx="7.35717" cy="7.35717" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="15.9939" cy="7.35717" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="24.6482" cy="7.35717" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="7.35717" cy="16.0002" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="15.9939" cy="16.0002" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="24.6482" cy="16.0002" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="7.35717" cy="24.6428" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="15.9939" cy="24.6428" rx="2.35717" ry="2.35717" fill="currentColor" />
-      <ellipse cx="24.6482" cy="24.6428" rx="2.35717" ry="2.35717" fill="currentColor" />
+    <svg :width="width" :height="height" :viewBox="'0 0 ' + width + ' ' + height" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <ellipse v-for="position in positions" :key="position" :cx="position.cx" :cy="position.cy" :rx="position.rx" :ry="position.ry" :fill="position.color" />
     </svg>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      width: {
-        type: Number,
-        default: 32,
-      },
-      height: {
-        type: Number,
-        default: 32,
-      },
-      color2: {
-        type: String,
-        default: "#000000",
-      },
-      color1: {
-        type: String,
-        default: "#FFFFFF",
-      },
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 32,
     },
-  };
+    height: {
+      type: Number,
+      default: 32,
+    },
+    color1: {
+      type: String,
+      default: "#FFFFFF",
+    },
+    color2: {
+      type: String,
+      default: "#000000",
+    },
+  },
+  data() {
+    return {
+      positions: [
+        { cx: 7.35717, cy: 7.35717, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 15.9939, cy: 7.35717, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 24.6482, cy: 7.35717, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 7.35717, cy: 16.0002, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 15.9939, cy: 16.0002, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 24.6482, cy: 16.0002, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 7.35717, cy: 24.6428, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 15.9939, cy: 24.6428, rx: 2.35717, ry: 2.35717, color: this.color1 },
+        { cx: 24.6482, cy: 24.6428, rx: 2.35717, ry: 2.35717, color: this.color1 },
+      ],
+    };
+  },
+  watch: {
+    color1(newVal) {
+      this.positions.forEach(position => {
+        position.color = newVal;
+      });
+    },
+    color2(newVal) {
+      this.positions.forEach((position, index) => {
+        position.color = index % 2 === 0 ? newVal : this.color1;
+      });
+    },
+  },
+};
 </script>
