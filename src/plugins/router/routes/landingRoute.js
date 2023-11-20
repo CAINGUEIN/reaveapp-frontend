34059c2f12df181aa38caf6048646dbdaa7f0537
Esteban <59009896+esteb621@users.noreply.github.com>
2023-11-20 12:46:00 +0100

    Revert "Change layout if not connected"
    This reverts commit 27844ce34c109b98f31fccde5c85793adcb6b9db.

--- src/plugins/router/routes/landingRoute.js
+++ src/plugins/router/routes/landingRoute.js
@@ -3,8 +3,6 @@ export default [
     path: "/home",
     name: "Landing",
     component: () => import("@components/events/Events.vue"),
-    meta: {
-      layout: localStorage.getItem("userSession") ? "Reave" : "Visitor",
-    },
+    meta: { layout: "Reave", permission: "log" },
   },
 ];
